7994a8ab9a9182d7b851617c8fe9ad8352929bdb
Imran Jami <imranajami@gmail.com>
2023-01-06 10:40:19 -0800

    test

---
+++ components/Splash/AnimatedPalette.tsx
@@ -0,0 +1,59 @@
+import styles from './AnimatedPalette.module.scss';
+import { motion } from 'framer-motion';
+
+const container = {
+  hidden: { opacity: 0 },
+  show: {
+    opacity: 1,
+    transition: {
+      delayChildren: 0.8,
+      staggerChildren: 0.2
+    }
+  }
+};
+
+const item = {
+  hidden: {
+    opacity: 0,
+    scale: 0,
+    rotate: -270
+  },
+  show: {
+    opacity: 1,
+    rotate: 0,
+    scale: 1,
+    transition: { duration: 0.6, type: 'spring', bounce: 0.3 }
+  }
+};
+
+const background = {
+  hidden: { opacity: 0 },
+  show: { opacity: 1, transition: { delay: 3.4 } }
+};
+
+const squares = [
+  { key: 1, color: '#febad1' },
+  { key: 2, color: '#fa0020' },
+  { key: 3, color: '#fdcf29' },
+  { key: 4, color: '#96ddef' },
+  { key: 5, color: '#b2dc27' }
+];
+
+const squareSvg = (width = 25, color = 'currentColor', key: number) => (
+  <motion.svg variants={item} key={key} viewBox='0 0 100 100' xmlns='http://www.w3.org/2000/svg' height={`${width}px`} width={`${width}px`}>
+    <rect x='0' y='0' width='100' height='100' rx='20' ry='20' fill={`${color}`} />
+  </motion.svg>
+);
+
+function AnimatedPalette() {
+  return (
+    <div className={styles.container}>
+      <motion.div variants={container} initial='hidden' animate='show' className={styles.palette}>
+        {squares.map((square) => squareSvg(20, square.color, square.key))}
+        <motion.div className={styles.background} variants={background} initial='hidden' animate='show' />
+      </motion.div>
+    </div>
+  );
+}
+
+export default AnimatedPalette;
--- components/Splash/Splash.tsx
+++ components/Splash/Splash.tsx
@@ -4,35 +4,41 @@ import classNames from 'classnames';
 import CurvedBorder from '../CurvedBorder';
 import JamiLogo from '../JamiLogo';
 import { motion, useDragControls } from 'framer-motion';
+import AnimatedPalette from './AnimatedPalette';
 
 interface Props {
   onClick?: () => void;
 }
 function Splash({ onClick }: Props) {
-  const variants = {
-    visible: { opacity: 1, scale: 1, transition: { duration: 1, type: 'spring' } },
-    hidden: { opacity: 0, scale: 0.6, transition: { duration: 1 } }
+  const logo = {
+    hidden: { opacity: 0, scale: 0, transition: { duration: 1 } },
+    visible: { opacity: 1, scale: 1, transition: { duration: 1, type: 'spring' } }
   };
 
-  // const controls = useDragControls();
+  const container = {
+    hidden: { opacity: 0, scale: 0.6, transition: { duration: 1 } },
+    visible: { opacity: 1, scale: 1, transition: { duration: 1, type: 'spring', delay: 3.4 } }
+  };
 
-  // function startDrag(event: any) {
-  //   controls.start(event, { snapToCursor: true });
-  // }
+  const prompt = {
+    hidden: { opacity: 0 },
+    visible: { opacity: 1, transition: { duration: 1, type: 'spring', delay: 2 } }
+  };
+
   const borders = (
     <div className={styles.borderContainer}>
-      <motion.div initial='hidden' animate='visible' exit='hidden' variants={variants} className={classNames(styles.border, styles.topRight)}>
+      <motion.div variants={container} initial='hidden' animate='visible' className={classNames(styles.border, styles.topRight)}>
         <CurvedBorder />
       </motion.div>
-      <div className={classNames(styles.border, styles.topLeft)}>
+      <motion.div variants={container} initial='hidden' animate='visible' className={classNames(styles.border, styles.topLeft)}>
         <CurvedBorder />
-      </div>
-      <div className={classNames(styles.border, styles.bottomRight)}>
+      </motion.div>
+      <motion.div variants={container} initial='hidden' animate='visible' className={classNames(styles.border, styles.bottomRight)}>
         <CurvedBorder />
-      </div>
-      <div className={classNames(styles.border, styles.bottomLeft)}>
+      </motion.div>
+      <motion.div variants={container} initial='hidden' animate='visible' className={classNames(styles.border, styles.bottomLeft)}>
         <CurvedBorder />
-      </div>
+      </motion.div>
     </div>
   );
 
@@ -41,8 +47,21 @@ function Splash({ onClick }: Props) {
   return (
     <main className={styles.main} onClick={onClick} ref={constraintsRef}>
       {borders}
-      <motion.div drag dragConstraints={constraintsRef} dragMomentum={false} dragElastic={0} whileDrag={{ scale: 0.9 }} initial='hidden' animate='visible' exit='hidden' variants={variants}>
+      {/* <motion.div drag dragConstraints={constraintsRef} dragMomentum={false} dragElastic={0} whileDrag={{ scale: 0.9 }} initial='hidden' animate='visible' exit='hidden' variants={logo}> */}
+      <motion.div className={styles.logo} initial='hidden' animate='visible' exit='hidden' variants={logo}>
         <JamiLogo />
+      </motion.div>
+      <AnimatedPalette />
+      <motion.div
+        animate={{
+          opacity: [0, 1, 0]
+          // scale: [1, 1.1, 1]
+        }}
+        transition={{ delay: 2, repeat: Infinity, duration: 1.4 }}
+        variants={prompt}
+        className={styles.prompt}
+      >
+        ( tap to enter )
       </motion.div>
     </main>
   );
