10961bb7205a4d1e0af56727ce15cd974bbdf9f0
Imran Jami <imranajami@gmail.com>
2023-01-11 00:25:07 -0800

    test

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,40 +1,28 @@
 import { AnimatePresence } from 'framer-motion';
 import Head from 'next/head';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { useMedia } from 'react-use';
 import Desktop from '../components/Desktop/Desktop';
 import Gameboy from '../components/Gameboy/Gameboy';
 import Splash from '../components/Splash/Splash';
-import styles from './index.module.scss';
-import css from 'styled-jsx/css';
 import { useTheme } from '../hooks/useTheme';
 
 export default function Home() {
-  const styles = css`
-    body {
-      &.LIGHT {
-        --primary-color: $black;
-        --secondary-color: $white;
-      }
-
-      &.DARK {
-        --primary-color: $white;
-        --secondary-color: $black;
-      }
-    }
-  `;
+  const isMobile = useMedia('(max-width: 600px)') || false;
 
   const [showSplash, setShowSplash] = useState(true);
 
-  const isMobile = useMedia('(max-width: 600px)') || false;
   const { theme } = useTheme();
+
+  useEffect(() => {
+    if (isMobile) setShowSplash(false);
+  }, []);
 
   return (
     <>
       <Head>
         <title>Home</title>
       </Head>
-      <style jsx>{styles}</style>
       <AnimatePresence mode='wait'>
         {showSplash ? (
           <Splash
