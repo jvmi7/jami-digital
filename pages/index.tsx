8a776fd27737eba6ab7a0861f3bc728ed4bc6063
Imran Jami <imranajami@gmail.com>
2023-02-02 16:12:42 -0800

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -41,7 +41,7 @@ export default function Home() {
               }}
             />
           ) : (
-            <Desktop key='desktop' />
+            <Gameboy />
           )}
         </AnimatePresence>
       </Div100vh>
