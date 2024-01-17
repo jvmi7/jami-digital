71adef9dc9744699a2b51cb06fefaaea06e5b9e7
Imran Jami <imranajami@gmail.com>
2024-01-16 23:10:22 -0800

    d

--- components/Mobile/BioSection/Messages.tsx
+++ components/Mobile/BioSection/Messages.tsx
@@ -1,7 +1,8 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import styles from './Messages.module.scss';
 import Image from 'next/image';
 import { delay, motion } from 'framer-motion';
+import { useInView } from 'react-intersection-observer';
 
 const messages = [
   <>
@@ -35,11 +36,27 @@ const messages = [
   </>,
 ];
 const Messages = () => {
-  // const [currentMessages, setCurrentMessages] = useState<React.ReactNode[]>([
-  //   messages[0],
-  // ]);
+  const [currentMessages, setCurrentMessages] = useState<React.ReactNode[]>([]);
 
-  const currentMessages = messages;
+  const { ref, inView } = useInView({
+    triggerOnce: false,
+    threshold: 1,
+  });
+
+  useEffect(() => {
+    if (inView) {
+      // add the first message
+      setCurrentMessages([messages[0]]);
+      // add the second message after a delay
+      setTimeout(() => {
+        setCurrentMessages([messages[0], messages[1]]);
+      }, 1500);
+      // add the third message after a delay
+      setTimeout(() => {
+        setCurrentMessages([messages[0], messages[1], messages[2]]);
+      }, 3000);
+    }
+  }, [inView]);
 
   return (
     <div className={styles.container}>
@@ -58,7 +75,7 @@ const Messages = () => {
               <div className={styles.space} />
             </div>
           ))}
-        <TypingIndicator />
+        {currentMessages.length < 3 && <TypingIndicator ref={ref} />}
       </div>
     </div>
   );
@@ -83,35 +100,39 @@ const ChatBubble = ({ index, message, isLast }: ChatBubbleProps) => {
   );
 };
 
-const TypingIndicator = () => {
-  const initial = { opacity: 0.5 };
-  const animate = { opacity: 1 };
+interface TypingIndicatorProps {}
 
-  return (
-    <div className={styles.typingIndicator}>
-      {Array.from(Array(3).keys()).map((_, index) => {
-        const delay = index * 0.15;
+const TypingIndicator = React.forwardRef<HTMLDivElement, TypingIndicatorProps>(
+  (props, ref) => {
+    const initial = { opacity: 0.5 };
+    const animate = { opacity: 1 };
 
-        const transition = {
-          duration: 0.6,
-          repeat: Infinity,
-          repeatDelay: 0.15 * 2,
-          ease: 'easeInOut',
-          delay: delay,
-        };
+    return (
+      <div ref={ref} className={styles.typingIndicator}>
+        {Array.from(Array(3).keys()).map((_, index) => {
+          const delay = index * 0.15;
 
-        return (
-          <motion.div
-            className={styles.dot}
-            initial={initial}
-            animate={animate}
-            transition={transition}
-            key={index}
-          />
-        );
-      })}
-    </div>
-  );
-};
+          const transition = {
+            duration: 0.6,
+            repeat: Infinity,
+            repeatDelay: 0.15 * 2,
+            ease: 'easeInOut',
+            delay: delay,
+          };
+
+          return (
+            <motion.div
+              className={styles.dot}
+              initial={initial}
+              animate={animate}
+              transition={transition}
+              key={index}
+            />
+          );
+        })}
+      </div>
+    );
+  }
+);
 
 export { Messages };
