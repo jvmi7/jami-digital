238a7e08556260a4aeb3e92234e0f01f161789a1
Imran Jami <imranajami@gmail.com>
2024-04-18 20:43:01 -0700

    Update index.tsx

--- components/windows/MotorheadzWindow/index.tsx
+++ components/windows/MotorheadzWindow/index.tsx
@@ -9,7 +9,8 @@ import styles from '../window.module.scss';
 import { ContentPane, ContentPaneMetadataType } from '../../Collection/ContentPane';
 import { Caption } from '../../Caption/Caption';
 import { ExternalLinkIcon } from '../../../icons/ExternalLinkIcon';
-import { useQuery } from 'react-query';
+import { useQuery } from '@tanstack/react-query';
+
 
 const fetchMetadata = async () => {
   const url = '/metadata/motorheadzMetadata.json'; // Replace with your JSON file's URL
